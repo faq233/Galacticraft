f574158028b6abc0db0d230b7b296b53424aba14
Johann Bernhardt <johann.bernhardt@tum.de>
2021-11-18 03:38:11 +0100

    Further trim dependencies

--- src/main/java/micdoodle8/mods/galacticraft/miccore/mixinplugin/TargetedMod.java
+++ src/main/java/micdoodle8/mods/galacticraft/miccore/mixinplugin/TargetedMod.java
@@ -11,7 +11,7 @@ public enum TargetedMod {
     // Replace with your injected mods here, but always keep VANILLA:
     VANILLA("Minecraft", "unused", true),
     FORGE("Forge", "unused", true),
-    PLAYER_API("Player-API", "Player-API", true),
+    PLAYER_API("Player-API", "Player-API", false),
     OPTIFINE("Optifine", "OptiFine_1.7.10", false),
     SYNCMOD("Sync Mod", "sync", false);
 

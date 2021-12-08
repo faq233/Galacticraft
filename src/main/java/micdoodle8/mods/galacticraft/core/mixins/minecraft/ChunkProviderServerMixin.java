c92f59ae83f413cc23790fa9c7c36e7220931df9
bombcar <github@bombcar.com>
2021-12-07 19:46:57 -0600

    add back trees (#37)

--- src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/ChunkProviderServerMixin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/ChunkProviderServerMixin.java
@@ -3,39 +3,22 @@ package micdoodle8.mods.galacticraft.core.mixins.minecraft;
 import cpw.mods.fml.common.registry.GameRegistry;
 import micdoodle8.mods.galacticraft.core.util.WorldUtil;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.ChunkProviderServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ChunkProviderServer.class)
 public class ChunkProviderServerMixin {
-    
-    @Shadow
-    public IChunkProvider currentChunkProvider;
 
-    @Shadow
-    public WorldServer worldObj;
-
-    @Unique
-    private boolean skipRegistryCalls = false;
-    
-    @Redirect(method = "populate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/IChunkProvider;populate(Lnet/minecraft/world/chunk/IChunkProvider;II)V"), require = 1)
-    private void onPopulate(IChunkProvider instance, IChunkProvider chunkProvider, int chunkX, int chunkZ) {
-        skipRegistryCalls = WorldUtil.otherModPreventGenerate(chunkX, chunkZ, worldObj, currentChunkProvider, chunkProvider);
-    }
-
-    @Redirect(method = "populate",
+    @Redirect(method = "populate(Lnet/minecraft/world/chunk/IChunkProvider;II)V",
             at = @At(value = "INVOKE",
                     target = "Lcpw/mods/fml/common/registry/GameRegistry;generateWorld(IILnet/minecraft/world/World;Lnet/minecraft/world/chunk/IChunkProvider;Lnet/minecraft/world/chunk/IChunkProvider;)V",
                     remap = false),
             require = 1)
     private void onRegistry(int chunkX, int chunkZ, World world, IChunkProvider currentChunkProvider, IChunkProvider chunkGenerator) {
-        if(skipRegistryCalls == false) {
+        if(!WorldUtil.otherModPreventGenerate(chunkX, chunkZ, world, currentChunkProvider, chunkGenerator)) {
             GameRegistry.generateWorld(chunkX, chunkZ, world, currentChunkProvider, chunkGenerator);
         }
     }
